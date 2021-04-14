6b5a738a96dc44e60a2113ddc238f9b6c32e02d5
Spencer Lazzar <spencerlazzar@yahoo.com>
2021-04-14 15:48:27 -0700

    Embedded Map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Main from './components/Main';
+import Map from './components/Map';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Map />
     </div>
   );
 }
