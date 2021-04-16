1ad1f1920062a0cc5cc5fd94f22fc2e98a3e4692
Spencer Lazzar <spencerlazzar@yahoo.com>
2021-04-16 09:54:09 -0700

    Added WindAlert

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Map from './components/Map';
 import Header from './components/Header';
+import NewcastleForcast from './components/NewcastleForcast';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Map />
+      <NewcastleForcast />
     </div>
   );
 }
---
+++ src/components/NewcastleForcast.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewcastleForcast = () => {
+  return (
+    <div>
+      <iframe align="top" src="https://widgets.windalert.com/widgets/web/modelTable?spot_id=1224&units_wind=mph&units_temp=F&type=daily&width=700&height=310&color=870100&name=Newcastle Park&app=windalert" width="700" height="310" frameborder="0" scrolling="no" allowtransparency="no"></iframe>
+    </div>
+  )
+}
+
+export default NewcastleForcast
