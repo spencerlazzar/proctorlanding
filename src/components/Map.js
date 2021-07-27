c953ad38f0dd0b10009467b3c824eaa8052d7253
Spencer Lazzar <spencerlazzar@yahoo.com>
2021-07-26 22:15:30 -0700

    Bootstrap up and running

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,14 +1,17 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 
 const Main = () => {
+  return (
+    <div>
+      <iframe
+        width='650'
+        height='450'
+        src='https://embed.windy.com/embed2.html?lat=47.587&lon=-122.255&detailLat=47.588&detailLon=-122.257&width=650&height=450&zoom=11&level=surface&overlay=wind&product=ecmwf&menu=&message=&marker=true&calendar=now&pressure=true&type=map&location=coordinates&detail=&metricWind=mph&metricTemp=%C2%B0F&radarRange=-1'
+        frameborder='0'
+      ></iframe>
+      <p className='text-uppercase'>I like cheese</p>
+    </div>
+  );
+};
 
-
-
-    return (
-        <div>
-            <iframe width="650" height="450" src="https://embed.windy.com/embed2.html?lat=47.587&lon=-122.255&detailLat=47.588&detailLon=-122.257&width=650&height=450&zoom=11&level=surface&overlay=wind&product=ecmwf&menu=&message=&marker=true&calendar=now&pressure=true&type=map&location=coordinates&detail=&metricWind=mph&metricTemp=%C2%B0F&radarRange=-1" frameborder="0"></iframe>
-        </div>
-    )
-}
-
-export default Main
+export default Main;
